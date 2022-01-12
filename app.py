a7e83a079a7a2eb1f86acf28d0b9a2f7a5a33c72
hannaheleen <71006734+hannaheleen@users.noreply.github.com>
2022-01-12 22:05:27 +0100

    Add files via upload
    Update recommendation api

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
 from flask import Flask
 from flask_restful import Api, Resource
+import random
 
 app = Flask(__name__)
 api = Api(app)
+
 
 class HelloWorld(Resource):
     def get(self):
         return {"data": "Hello World"}
 
+
 api.add_resource(HelloWorld, "/helloWorld")
 
+recom_sad = ["Go and take a break!", "recommendation sad 2", "recommendation sad 3"]
+recom_happy = ["Write down the reason", "recommendation happy 2", "recommendation happy 3"]
+recom_angry = ["Watch this video: XYZ", "recommendation angry 2", "recommendation angry 3"]
+recom_confused= []
+recom_disgusted= []
+recom_suprised= []
+recom_calm = []
+recom_unknown= []
+recom_fear= []
+
+class Emotion(Resource):
+    def get(self, emotion):
+        if emotion == "happy":
+            return {"answer": random.choice(recom_happy)}
+        if emotion == "sad":
+            return {"answer": random.choice(recom_sad)}
+        if emotion == "angry":
+            return {"answer": random.choice(recom_angry)}
+        if emotion == "confused":
+            return {"answer": random.choice(recom_confused)}
+        if emotion == "disgusted":
+            return {"answer": random.choice(recom_disgusted)}
+        if emotion == "suprised":
+            return {"answer": random.choice(recom_suprised)}
+        if emotion == "calm":
+            return {"answer": random.choice(recom_calm)}
+        if emotion == "unknown":
+            return {"answer": random.choice(recom_unknown)}     
+        if emotion == "fear":
+            return {"answer": random.choice(recom_fear)}
+
+api.add_resource(Emotion, "/recom/<string:emotion>")
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
