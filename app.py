991314a8c924dbe48c4d2669155b799f3df6e394
Viet Duc Kieu <viet.duc.kieu@web.de>
2022-01-20 00:00:43 +0100

    added flask cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 import random
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
@@ -13,15 +15,19 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, "/helloWorld")
 
-recom_sad = ["Go and take a break!", "Watch this video on Youtube: https://youtu.be/0Bmhjf0rKe8", "Watch this video on Youtube: https://youtu.be/pNtNxMt_b7I"]
-recom_happy = ["Write down the reason", "recommendation happy 2", "recommendation happy 3"]
-recom_angry = ["Watch this video: https://youtu.be/hbH53mfC24c", "Let's do this workout: https://www.tk.de/techniker/magazin/sport/spezial/gesunder-ruecken/trainingsflaeche-buero/8-minuten-workout-fuers-buero-2009264", "recommendation angry 3"]
-recom_confused= []
-recom_disgusted= []
-recom_suprised= []
+recom_sad = ["Go and take a break!", "Watch this video on Youtube: https://youtu.be/0Bmhjf0rKe8",
+             "Watch this video on Youtube: https://youtu.be/pNtNxMt_b7I"]
+recom_happy = ["Write down the reason",
+               "recommendation happy 2", "recommendation happy 3"]
+recom_angry = ["Watch this video: https://youtu.be/hbH53mfC24c",
+               "Let's do this workout: https://www.tk.de/techniker/magazin/sport/spezial/gesunder-ruecken/trainingsflaeche-buero/8-minuten-workout-fuers-buero-2009264", "recommendation angry 3"]
+recom_confused = []
+recom_disgusted = []
+recom_suprised = []
 recom_calm = []
-recom_unknown= []
-recom_fear= []
+recom_unknown = []
+recom_fear = []
+
 
 class Emotion(Resource):
     def get(self, emotion):
@@ -40,9 +46,10 @@ class Emotion(Resource):
         if emotion == "calm":
             return {"answer": random.choice(recom_calm)}
         if emotion == "unknown":
-            return {"answer": random.choice(recom_unknown)}     
+            return {"answer": random.choice(recom_unknown)}
         if emotion == "fear":
             return {"answer": random.choice(recom_fear)}
+
 
 api.add_resource(Emotion, "/recom/<string:emotion>")
 
